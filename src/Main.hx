956a17ca885a7e820b777d12b7ac25b1e8793649
Sami <puggsoy@gmail.com>
2015-08-27 01:47:19 +1200

    Documentation and a bit more printy stuff

--- src/Main.hx
+++ src/Main.hx
@@ -6,21 +6,30 @@ import haxe.io.Path;
 import sys.FileSystem;
 import systools.Dialogs;
 
+/**
+ * Main class. Initialises program, takes input.
+ */
 class Main 
 {
-	static private inline var TITLE:String = "VGSC v2.1 - by puggsoy (puggsoy.wordpress.com)\nUses vgmstream r1040 - by hcs (www.hcs64.com)\n==========\n";
+	static private inline var TITLE:String = "==========\nVGSC v2.1 - by puggsoy (puggsoy.wordpress.com)\nUses vgmstream r1040 - by hcs (www.hcs64.com)\n==========\n";
 	static private inline var USAGE:String = "Command line usage: VGSC inDir outDir\n    inDir: Path to directory containing files to convert\n    outDir: Path to directory where converted files will be saved";
 	
 	static private var _args:Array<String>;
 	
+	/**
+	 * Entry point. Initialise stuff, parse arguments.
+	 */
 	static function main() 
 	{
 		Begin.init();
 		Begin.usage = USAGE;
-		Begin.functions = [selectFiles, null, parseArgs];
+		Begin.functions = [selectFiles, null, parseArgs]; //0 arguments, show UI. Needs 2 otherwise.
 		Begin.parseArgs();
 	}
 	
+	/**
+	 * Called when no arguments are passed in. Gives selection windows for input files and output directory.
+	 */
 	static private function selectFiles()
 	{
 		Sys.println(TITLE);
@@ -49,23 +58,29 @@ class Main
 		convert(files, outDir);
 	}
 	
+	/**
+	 * Called when 2 or more arguments are passed in. Parses first argument as input directory, second as output directory. Both must exist.
+	 */
 	static private function parseArgs()
 	{
+		Sys.println(TITLE);
+		
 		var inDir:String = new Path(Begin.args[0]).toString();
 		
 		if (!FileSystem.exists(inDir) || !FileSystem.isDirectory(inDir))
 		{
-			End.anyKeyExit(3, "First argument must be an existing directory");
+			End.anyKeyExit(3, "First argument must be an existing directory!");
 			return;
 		}
 		
 		inDir = FileSystem.fullPath(inDir);
+		Sys.println('Input folder: $inDir');
 		
 		var inFiles:Array<String> = FileSystem.readDirectory(inDir);
 		
 		if (inFiles.length == 0)
 		{
-			End.anyKeyExit(3, "First argument must contain files");
+			End.anyKeyExit(3, "Input folder must contain files!");
 			return;
 		}
 		
@@ -83,10 +98,16 @@ class Main
 		}
 		
 		outDir = FileSystem.fullPath(outDir);
+		Sys.println('Output folder: $outDir');
 		
 		convert(inFiles, outDir);
 	}
 	
+	/**
+	 * Converts the files using VGMStream.
+	 * @param	fileList    List of input files.
+	 * @param	outDir      Output directory.
+	 */
 	static private function convert(fileList:Array<String>, outDir:String)
 	{
 		VGMStream.exePath = "vgmstream/vgmstream.exe";
--- src/VGMStream.hx
+++ src/VGMStream.hx
@@ -6,10 +6,22 @@ import haxe.io.Path;
 import sys.io.Process;
 import systools.win.Tools;
 
+/**
+ * Class that interacts with vgmstream.
+ */
 class VGMStream
 {
+	/**
+	 * The relative path to the vgmstream .exe. Must be set before calling convert.
+	 */
 	static public var exePath(default, default):String;
 	
+	/**
+	 * Converts a file. Can either convert it to an external file directly, or get the file data directly as a WAVE object (from the format library).
+	 * @param	path      Absolute path of the file to convert.
+	 * @param	outDir    The directory to save the converted file in. If this is null, it will return the file data as a WAVE.
+	 * @return            The file data WAVE. If outDir != null (file is saved externally) then this returns null.
+	 */
 	static public function convert(path:String, outDir:String = null):WAVE
 	{
 		if (exePath == null)
@@ -20,7 +32,7 @@ class VGMStream
 		
 		var inFile:String = "\"" + path + "\"";
 		
-		if (outDir == null)
+		if (outDir == null) //Get file data as a WAVE
 		{
 			var process:Process = new Process(exePath, ["-p", "-i", inFile]);
 			
@@ -31,6 +43,7 @@ class VGMStream
 			return inWav;
 		}
 		
+		//Otherwise let vgmstream save it
 		var fileName:String = new Path(path).file;
 		var outFile:String = "\"" + new Path(outDir).toString() + "\\" + fileName + ".wav\"";
 		
